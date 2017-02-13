10d5a42608278f5805c1fed40ae48a290bf5742c
Carter Jiang <yejiangholy@gmail.com>
2017-02-13 18:51:55 -0500

    fist implementation

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
-from flask import Flask
+from flask import Flask,jsonify
 
 app = Flask(__name__)
 
+# jason is mainly a dictionary in a long string format , which is useful to transfer data
+# A REST API returns json after some processing
+#stores is a list of dictionary
+
+stores = [
+    {
+        "name":"my store",
+        'items':[
+            {
+                "name":"item1",
+                'price':17.99
+            },
+            {
+                "name":"item2",
+                "price":39.00
+            }
+
+        ]
+    }
+]
+
 #POST /store data: {name:}
-@app.route('/store', methods = ['POST'])
+@app.route('/store', methods =['POST'])
 def create_store():
     pass
 
@@ -16,8 +37,8 @@ def get_store(name):
 
 #GET /store/
 @app.route('/store')
-def get_store():
-    pass
+def get_stores():
+    return jsonify({'stores':stores})
 
 
 #POST /store/<string:name>/item {name: , price:}
