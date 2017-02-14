df1e88d90343a3d589a437a16d3bafa7ab474b41
Carter Jiang <yejiangholy@gmail.com>
2017-02-14 17:18:14 -0500

    first two resources in our API

--- REST_API/code/app.py
+++ REST_API/code/app.py
@@ -1,15 +1,39 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource,Api
 
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
+items = []  # item list
+
+
+class Item(Resource):
     def get(self,name):
-        return {"student":name}
+        for item in items:
+            if item["name"] == name:
+                return item
+        return {"item":None},404
 
 
-api.add_resource(Student,"/student/<string:name>") # http://127.0.0.1:5000/studnt/Rolf
+    def post(self,name):
 
-app.run(port=5000)
+        data = request.get_json(silent = True)
+        price = data["price"]
+        item = {"name": name, "price": price}
+        items.append(item)
+        return item, 201 # creating success # 201 ||  # 202 --> accepted but not excuted
+
+
+
+class ItemList(Resource):
+    def get(self):
+        return {"items": items}
+
+
+
+api.add_resource(Item,"/item/<string:name>")
+api.add_resource(ItemList, "/items")
+
+
+app.run(port=5000, debug=True)
 
