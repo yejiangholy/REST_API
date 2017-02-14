f3825087c48ae4f954d1e0491f6de4128a2c0fe8
Carter Jiang <yejiangholy@gmail.com>
2017-02-13 21:40:34 -0500

    other end points implementation

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
 
 app = Flask(__name__)
 
@@ -9,7 +9,7 @@ app = Flask(__name__)
 stores = [
     {
         "name":"my store",
-        'items':[
+        "items":[
             {
                 "name":"item1",
                 'price':17.99
@@ -26,13 +26,22 @@ stores = [
 #POST /store data: {name:}
 @app.route('/store', methods =['POST'])
 def create_store():
-    pass
+    request_data = request.get_json()
+    new_store = {
+        "name":request_data["name"],
+        "items":[]
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
 
 
 #GET /store/<string:name>
 @app.route('/store/<string:name>')
 def get_store(name):
-    pass
+    for store in stores:
+        if store["name"] == name:
+            return jsonify(store)
+    return jsonify({"message":"store not exist"})
 
 
 #GET /store/
@@ -44,12 +53,30 @@ def get_stores():
 #POST /store/<string:name>/item {name: , price:}
 @app.route('/store/<string:name>/item', methods = ['POST'])
 def create_store_inStore(name):
-    pass
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_Item = {
+                "name": request_data["name"],
+                "price":request_data["price"]
+            }
+            store["item"].append(new_Item)
+            return jsonify(new_Item)
+
+    return jsonify({"message":"store not exist"})
+
+
+
+
 
 #GET /store/<string:name>/item
 @app.route('/store/<string:name>/item')
 def get_items_inStore(name):
-    pass
+    for store in stores:
+        if store["name"] == name:
+            return jsonify({"items":store["items"]})
+
+    return jsonify({"message":"store not exist"})
 
 
 app.run(port=5000)
