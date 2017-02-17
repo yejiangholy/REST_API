058cbb739bb5968f34927d431254a92d48b76bce
Carter Jiang <yejiangholy@gmail.com>
2017-02-17 13:31:43 -0500

    get JWT access token back from /auth endPoint

--- REST_API/code/app.py
+++ REST_API/code/app.py
@@ -1,19 +1,20 @@
 from flask import Flask, request
 from flask_restful import Resource,Api
-from flask_jwt import  JWT
-from REST_API.code.security import authenticate, identify
+from flask_jwt import JWT , jwt_required
+from REST_API.code.security import authenticate, identity
 
 app = Flask(__name__)
 
 app.secret_key = "Secret"
 api = Api(app)
 
-jwt = JWT(app, authenticate, identify)  # /auth
+jwt = JWT(app, authenticate, identity)  # /auth
 
 items = []  # item list
 
 
 class Item(Resource):
+    @jwt_required()
     def get(self,name):
         # for item in items:
         #     if item["name"] == name:
--- REST_API/code/security.py
+++ REST_API/code/security.py
@@ -10,13 +10,18 @@ username_mapping = { u.username: u for u in users}
 userid_mapping = { u.id: u for u in users }
 
 def authenticate(username, password):
-    user = username_mapping(username, None) # default is None
+    user = username_mapping.get(username, None) # default is None
 
     if user and safe_str_cmp(user.password , password):
         return user
     else: return None
 
 
-def identify(payload):
+def identity(payload):
     user_id = payload["identity"]
     return userid_mapping.get(user_id,None)
+
+
+# {
+#   "access_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0ODczNTYyMDMsImlkZW50aXR5IjoxLCJpYXQiOjE0ODczNTU5MDMsIm5iZiI6MTQ4NzM1NTkwM30.VHJ3zx5oIiIwuJXQ7H9hsQ6s6FS_8xOGSrQpTpNcauo"
+ # }
