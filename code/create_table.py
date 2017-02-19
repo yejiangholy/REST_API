6da9b4e3eef6170510e5809896f3715bab448a82
Carter Jiang <yejiangholy@gmail.com>
2017-02-19 17:41:45 -0500

    basic functions all work now

--- code/create_table.py
+++ code/create_table.py
@@ -10,8 +10,6 @@ cursor.execute(create_table)
 create_table = "CREATE TABLE IF NOT EXISTS items(name text, price real)"
 cursor.execute(create_table)
 
-cursor.execute("INSERT INTO items VALUES('testItem', 9.99)")
-
 
 connection.commit()
 connection.close()
--- code/item.py
+++ code/item.py
@@ -13,29 +13,47 @@ class Item(Resource):
                         help="This field cannot be left blank")
     #data = parser.parse_args()
     def get(self,name):
+       item = self.find_by_name(name)
+       if item:
+           return item
+       return {"message":"Item not found"},404
+
+
+
+    @classmethod
+    def find_by_name(cls,name):
         connetion = sqlite3.connect("data.db")
         cursor = connetion.cursor()
 
         query = "SELECT * FROM items WHERE name=?"
-        result = cursor.execute(query,(name,))
+        result = cursor.execute(query, (name,))
         row = result.fetchone()
         connetion.close()
 
         if row:
-            return {"item" : {"name": row[0], "price": row[1]}}
-        return {"message":"Item not found"},404
+            return {"item": {"name": row[0], "price": row[1]}}
 
 
     @jwt_required()
     def post(self,name):
-        if  next(filter(lambda x: x["name"] == name, items),None) is not None:
+        if self.find_by_name(name):
             return {"message":"An item with name  '{}' already exist.".format(name)},400 # bad request
 
 
         data = Item.parser.parse_args()
         price = data["price"]
         item = {"name": name, "price": price}
-        items.append(item)
+
+        connection = sqlite3.connect("data.db")    # 1. connect db create cursor
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES(?,?)"    # 2. create query and execute it
+        cursor.execute(query,(item["name"],item["price"]))
+
+        connection.commit()             #3. commit change and close connection
+        connection.close()
+
+
         return item, 201 # creating success # 201 ||  # 202 --> accepted but not excuted
 
     @jwt_required()
